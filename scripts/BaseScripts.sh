13c81ff3cb85724e82cdce7fac8f5e8e76ed49de
nn <37653895+codetiger666@users.noreply.github.com>
2022-01-30 08:05:13 +0800

    Update BaseScripts.sh

--- scripts/BaseScripts.sh
+++ scripts/BaseScripts.sh
@@ -17,6 +17,8 @@ Diy-Part1() {
     git clone https://github.com/jerrykuku/luci-theme-argon.git
     git clone https://github.com/KFERMercer/openwrt-baidupcs-web.git
     git clone https://github.com/zxl78585/luci-app-filetransfer.git
+    rm -rf luci-app-openclash
+    git clone https://github.com/vernesong/OpenClash.git -b dev
     echo "$KERNEL_HASH" > $GITHUB_WORKSPACE/openwrt/vermagic
     sed -i 's/${ipaddr:-"192.168.1.1"}/${ipaddr:-"10.10.1.1"}/g' $GITHUB_WORKSPACE/openwrt/package/base-files/files/bin/config_generate
     sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).1"}/${ipaddr:-"10.10.$((addr_offset++)).1"}/g' $GITHUB_WORKSPACE/openwrt/package/base-files/files/bin/config_generate
